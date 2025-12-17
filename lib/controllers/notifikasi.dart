e181d58a3ed2a4f50bc3136fc0d40a2deabec3a9
Andre Firmansyah <af349393@gmail.com>
2025-12-17 20:55:25 +0700

    update

--- lib/controllers/notifikasi.dart
+++ lib/controllers/notifikasi.dart
@@ -3,7 +3,6 @@ import 'package:gobox/controllers/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:gobox/model/notifikasi.dart';
-import 'http.dart'
 
 class NotificationService {
   final String baseUrl = httpss;
